809e92c6974b38bdda379da56201fc68078b2665
Bartosz Jarzyński <bartjar978@student.polsl.pl>
2025-03-21 18:15:23 +0100

    implement custom authentication backend to support login with email or username and update registration form to require email

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'sites.backends.EmailOrUsernameModelBackend'
+]
--- sites/views.py
+++ sites/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Post, Comment
 from .PostForm import PostForm
 from .CommentForm import CommentForm
 from .forms import PostSearchForm
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import login, logout
+from django import forms
+from django.contrib.auth.models import User
 
 def home_page(request):
     form = PostSearchForm(request.GET or None)
@@ -45,24 +45,41 @@ def add_comment(request, post_id):
         form = CommentForm(initial={'post': post})
     return render(request, 'add_comment.html', {'form': form, 'post': post})
 
+class UserCreationFormWithEmail(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationFormWithEmail, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationFormWithEmail(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home_page')
     else:
-        form = UserCreationForm()
+        form = UserCreationFormWithEmail()
     return render(request, 'register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home_page')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home_page')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
